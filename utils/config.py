1046833996fc40348900ac22e351905610ee4498
Amogh Badugu <badugu.amogh@gmail.com>
2023-09-05 11:20:29 +0530

    added config variables

--- utils/config.py
+++ utils/config.py
@@ -1,2 +1,8 @@
 from pathlib import Path
+import torch
+
 TIME_LOG_PATH = Path("./logs/time.log")
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+SCREENSHOT_MODEL = "google/pix2struct-screen2words-base"
+WEB_MODEL = "google/bart-large-cnn"
